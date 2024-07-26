c9dd461deb577f101fdb60b70fc85d5cbc9d738d
g0yujin <136612437+g0yujin@users.noreply.github.com>
2024-07-26 21:25:33 +0900

    [Silver V] Title: 너의 평점은, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,16 @@
+grade_score = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 'B0': 3.0, 'C+': 2.5, 'C0': 2.0, 'D+': 1.5, 'D0': 1.0, 'F': 0.0}
+score_sum = 0
+grade_sum = 0
+
+for i in range(20):
+    subject, score, grade = input().split()
+    if grade == 'P':
+        pass
+    else:
+        score_sum += float(score)
+        grade_sum += float(score) * grade_score[grade]
+
+
+print(format(grade_sum / score_sum, ".6f"))
+
+
