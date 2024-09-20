ebca458d3a961d4b2af691b05be3ae999acc3115
g0yujin <136612437+g0yujin@users.noreply.github.com>
2024-09-20 20:25:33 +0900

    [Silver IV] Title: 정수 제곱근, Time: 36 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
@@ -0,0 +1,17 @@
+import sys, math
+
+n = int(sys.stdin.readline())
+
+start = 0
+end = n
+
+while start <= end:
+    mid = (start+end) // 2
+
+    if mid ** 2 < n:
+        start = mid + 1
+
+    else:
+        end = mid - 1
+
+print(start)
