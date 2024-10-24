8d65c61b1b7c7f8b0fb73f1f919475c7a535df45
g0yujin <136612437+g0yujin@users.noreply.github.com>
2024-10-25 08:17:42 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -0,0 +1,11 @@
+def solution(x):
+    x = str(x)
+    
+    sum = 0
+    for i in range(len(x)):
+        sum += int(x[i])
+        
+    if int(x) % sum == 0:
+        return True
+    else:
+        return False
