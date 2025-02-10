e2f31f359747e60ea7a104063503914933a7b62c
g0yujin <136612437+g0yujin@users.noreply.github.com>
2025-02-10 22:43:52 +0900

    [D1] Title: 연월일 달력, Time: 48 ms, Memory: 49,664 KB -BaekjoonHub

--- SWEA/D1/2056. 연월일 달력/연월일 달력.py
+++ SWEA/D1/2056. 연월일 달력/연월일 달력.py
@@ -1,14 +1,15 @@
 T = int(input())
 
-calender = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+calendar = [31, 28, 31, 30, 31, 30, 31, 31, 30, 30, 31, 30, 31]
 
-for test_case in range(1, T+1):
+for t in range(1, T+1):
     date = input()
+
     year = date[0:4]
     month = date[4:6]
     day = date[6:8]
 
-    if 0 < int(year) and 0 < int(month) <= 12 and 0 < int(day) <= calender[int(month)-1]:
-        print(f'#{test_case} {year}/{month}/{day}')
+    if int(year) > 0 and 0 < int(month) <= 12  and 0 < int(day) <= calendar[int(month)-1]:
+        print(f'#{t} {year}/{month}/{day}')
     else:
-        print(f'#{test_case} -1')
+        print(f'#{t} -1')
