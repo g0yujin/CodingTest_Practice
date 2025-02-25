b0453b99e7ad5ee2926970869a5e637b9b279ef3
g0yujin <136612437+g0yujin@users.noreply.github.com>
2025-02-25 09:36:14 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        int result = 0;
+        
+        for(int i=0; i<included.length; i++){
+            if(included[i] == true){
+                result += a + (i*d);
+            }
+            
+        }
+        return result;
+    }
+}
