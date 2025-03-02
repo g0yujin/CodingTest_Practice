a55266ce628c794a86112f0237d8967591a2dec2
g0yujin <136612437+g0yujin@users.noreply.github.com>
2025-03-03 02:31:03 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 2,919 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,66 +1,86 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Solution{
-	static int N = 18, R = 9;							
-	static int[] gu;
-	static int[] in;
-	static int[] rem_card;
-	static boolean[] isSelected;
-	static int win;
+class Solution {
 
-	public static void game(int cnt) {
-		if (cnt == R) {
-			int gu_point = 0;
-			int in_point = 0;
-			for (int i = 0; i < R; i++) {
-				if (gu[i] > in[i])
-					gu_point += (gu[i] + in[i]);
-				else
-					in_point += (gu[i] + in[i]);
+	static int T, win, lose;
+	static int[] gyu, in, numbers;
+	static boolean[] exists, isSelected;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// 테스트 케이스만큼 반복
+		T = Integer.parseInt(br.readLine());
+		for(int tc=1; tc<T+1; tc++) {
+			
+			gyu = new int[9];
+			numbers = new int[9];
+			
+			//규영이의 카드 입력받기
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i=0; i<9; i++) {
+				gyu[i] = Integer.parseInt(st.nextToken());
 			}
-			if (gu_point > in_point)
-				win++;
+			
+			//인영이의 카드
+			exists = new boolean[19]; // 인덱스 1~18까지 사용 
+			// 규영이가 가진 카드의 숫자들을 true 표시
+			for(int g: gyu) {
+				exists[g] = true;
+			}
+			
+			int idx = 0;
+			for(int i=1; i<19; i++) {
+				if(!exists[i]) {
+					numbers[idx++] = i;
+				}
+			}
+			
+			// 순열
+			in = new int[9]; // 순열로 선택된 수 = 인영이가 낼 카드
+			isSelected = new boolean[9]; //쓰인 수
+			win = 0;
+			lose = 0;
+					
+			permu(0);
+			System.out.println("#" + tc + " " + win + " " + lose);
+			
+		}
+	}
+	// cnt: 몇 번째 자리인지
+	// i : 어떤 숫자를 선택할지
+	// 순열 함수
+	public static void permu(int cnt) {
+		if(cnt == 9) {
+			calc();
 			return;
 		}
-		for (int i = 0; i < R; i++) {
-			if (isSelected[i])
-				continue;
-			in[cnt] = rem_card[i];
+		
+		for(int i=0; i<9; i++) {
+			if(isSelected[i]) continue; // 이미 선택된 요소는 스킵
+			
+			in[cnt] = numbers[i];
 			isSelected[i] = true;
-			game(cnt + 1);
+			permu(cnt+1);
 			isSelected[i] = false;
 		}
 	}
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int tc = Integer.parseInt(br.readLine());
-		for (int T = 1; T <= tc; T++) {
-			win = 0;
-			gu = new int[9];
-			in = new int[9];
-			isSelected = new boolean[9];
-			rem_card = new int[9];
-
-			int idx = 0;
-			boolean[] cnt = new boolean[18];
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-			for (int i = 0; i < R; i++) {
-				gu[i] = Integer.parseInt(st.nextToken());
-				cnt[gu[i] - 1] = true;
-			}
-			for (int i = 0; i < N; i++) {
-				if (cnt[i])
-					continue;
-				else
-					rem_card[idx++] = i + 1;
-			}
-			game(0);
-			sb.append("#").append(T).append(" ").append(win).append(" ").append(362880 - win).append("\n");
+	
+	
+	// 누가 이기는지 계산하는 함수
+	public static void calc() {
+		int gyu_score = 0;
+		int in_score = 0;
+		
+		for(int i=0; i<9; i++) {
+			if(gyu[i] > in[i]) {gyu_score+= gyu[i]+in[i];} // 규영이가 낸 카드가 더 큰 경우
+			else if(in[i] > gyu[i]) {in_score += gyu[i]+in[i];} // 규영이가 낸 카드가 더 작은 경우
 		}
-		System.out.println(sb.toString());
+		if(gyu_score > in_score) {win += 1;}  // 규영이가 이기는 경우
+		else if(gyu_score < in_score) {lose += 1;} // 규영이가 지는 경우
+		
 	}
 }
