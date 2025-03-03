dd9da021c85fa89fa2bf320be3dc609604177b09
g0yujin <136612437+g0yujin@users.noreply.github.com>
2025-03-03 17:54:30 +0900

    [D2] Title: 파리 퇴치, Time: 146 ms, Memory: 48,504 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,24 +1,55 @@
-T = int(input())
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-for testcase in range(1, T+1):
-    N, M = map(int, input().split())
-    num = []
-    result = []
-    for i in range(N):
-        temp = list(map(int, input().split(' ')))
-        num.append(temp)
+class Solution {
+	static int T, N, M;
+	static int[][] graph;
+	static int maxFly; 
 
-
-    for i in range(N-M+1):
-        j = 0
-        while j+M-1 <= N-1:
-            hap = 0
-            for k in range(i, i+M):
-                hap += sum(num[k][j:j+M])
-
-            j += 1
-            result.append(hap)
-
-
-    print(f'#{testcase} {max(result)}')
-
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// 테스트케이스만큼 반복
+		T = Integer.parseInt(br.readLine());
+	
+		for(int tc=1; tc<T+1; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			maxFly = Integer.MIN_VALUE;
+			
+			// 배열 입력받기
+			graph = new int[N][N];
+			for(int i=0; i<N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j=0; j<N; j++) {
+					graph[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			getMaxFly();
+			System.out.println("#" + tc + " " + maxFly);
+		}
+		
+	}
+	
+	public static void getMaxFly() {
+		
+		
+		// 파리채가 배열의 범위를 벗어나지 않도록
+		for(int i=0; i<=N-M; i++) {
+			for(int j=0; j<=N-M; j++) {
+				int sum = 0;
+				
+				for(int x=0; x<M; x++) {
+					for(int y=0; y<M; y++){
+						sum += graph[i+x][j+y];
+					}
+				}
+				maxFly = Math.max(maxFly, sum);
+			}
+		}
+	}
+}
