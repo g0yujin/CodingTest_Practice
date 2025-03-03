6fa7b548cb4d72e8b229d16ac1c1a003b79eeabb
g0yujin <136612437+g0yujin@users.noreply.github.com>
2025-03-03 23:06:19 +0900

    [D3] Title: 햄버거 다이어트, Time: 741 ms, Memory: 27,264 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,33 +1,27 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Solution {
+	static int T, N, L, maxScore;
+	static int[] score, calori;
 	
-	static int T, N, limitCalori;
-	static int[] score, calori, selected;
-	static int maxScore; 
-	
-	
-
 	public static void main(String[] args) throws Exception{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// 테스트 케이스만큼 반복
+		
+		// 테스트케이스만큼 반복
 		T = Integer.parseInt(br.readLine());
+		
 		for(int tc=1; tc<T+1; tc++) {
-			
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());  // 재료의 수
-			limitCalori = Integer.parseInt(st.nextToken()); // 제한 칼로리
+			N = Integer.parseInt(st.nextToken());
+			L = Integer.parseInt(st.nextToken());
 			
-			// 재료별 맛과 칼로리 입력받기
+			
+			// 재료별 점수와 칼로리 입력받기
 			score = new int[N];
 			calori = new int[N];
-			selected = new int[N];
-			
-			maxScore = Integer.MIN_VALUE;
 			
 			for(int i=0; i<N; i++) {
 				st = new StringTokenizer(br.readLine());
@@ -35,40 +29,27 @@ class Solution {
 				calori[i] = Integer.parseInt(st.nextToken());
 			}
 			
-			// 조합 nCr에서 r을 1부터 n개 뽑는 조합 경우의 
-			for(int r=1; r<N+1; r++) {
-				combi(0, 0, r);
+			maxScore = 0;
+			// 비트마스킹으로 부분조합 2^N반복
+			for(int i=0; i<(1<<N); i++) {
+				int totalScore = 0;
+				int totalCalori = 0;
+				
+				// j번째 수가 1인지 확인
+				for(int j=0; j<N; j++) {
+					// j번째 수가 1이면 
+					if((i & (1<<j))!= 0) {
+						totalScore += score[j];
+						totalCalori += calori[j];
+					}
+				}
+				if(totalCalori <= L) {
+					maxScore = Math.max(maxScore, totalScore);
+				}
 			}
-			
 			System.out.println("#" + tc + " " + maxScore);
 		}
 		
 		
-	}
-	
-	public static void combi(int cnt, int start, int r) {
-		
-		// 기저조건: r개만큼 뽑았다면
-		if(cnt == r) {
-			//칼로리 계산
-			int totalCalori = 0;
-			int totalScore = 0;
-			
-			for(int i=0; i<r; i++) {
-				totalCalori += calori[selected[i]];
-				totalScore += score[selected[i]];
-				
-			}
-			if(totalCalori <= limitCalori) {
-				maxScore = Math.max(totalScore, maxScore);
-			}
-			return;
-		}
-		
-		for(int i=start; i<N; i++) {
-			selected[cnt] = i;
-			
-			combi(cnt+1,i+1, r);
-		}
 	}
 }
