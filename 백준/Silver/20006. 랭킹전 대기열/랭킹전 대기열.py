0e4a53e34d579af657727e38a9498aa1d1150219
g0yujin <136612437+g0yujin@users.noreply.github.com>
2025-07-24 00:03:24 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.py
@@ -1,8 +1,7 @@
 import sys
 
 P, M = map(int, sys.stdin.readline().split())
-
-rooms = []  # 각 방은 [기준레벨, [(레벨, 닉네임), ...]] 형태
+rooms = []
 
 for i in range(P):
     L, N = sys.stdin.readline().strip().split()
@@ -11,30 +10,28 @@ for i in range(P):
     # 들어갈 수 있는 방 찾기
     entered = False
     for room in rooms:
-        base_level = room[0]
-        players = room[1]
+        base_level = room[0][0]
 
         # 레벨 범위 확인 & 방 정원 확인
-        if (base_level - 10) <= L <= (base_level + 10) and len(players) < M:
-            players.append((L, N))
+        if (base_level - 10) <= L <= (base_level + 10) and len(room) < M:
+            room.append((L, N))
             entered = True
             break
 
     # 들어갈 방이 없으면 새 방 생성
     if not entered:
-        rooms.append([L, [(L, N)]])
+        rooms.append([(L, N)])
 
 # 결과 출력
 for room in rooms:
-    players = room[1]
-
     # 방 상태 출력
-    if len(players) == M:
+    if len(room) == M:
         print("Started!")
     else:
         print("Waiting!")
 
     # 플레이어 목록 (닉네임 순 정렬)
-    players.sort(key=lambda x: x[1])
-    for level, name in players:
+    room.sort(key=lambda x: x[1])
+    for level, name in room:
         print(level, name)
+
