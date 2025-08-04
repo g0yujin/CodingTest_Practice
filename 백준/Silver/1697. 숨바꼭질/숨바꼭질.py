06debb60c1469b8c5681fc562af51d4eee2fba01
g0yujin <136612437+g0yujin@users.noreply.github.com>
2025-08-04 23:39:09 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 112872 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -2,23 +2,22 @@ import sys
 from collections import deque
 
 N, K = map(int, sys.stdin.readline().split())
+max_num = 100000
+visited = [0] * (max_num + 1)
 
+def bfs():
+    q = deque()
+    q.append(N)
 
-d = deque()
+    while q:
+        x = q.popleft()
 
-d.append(N)
-visited = [-1] * 100001   # 각 위치까지 도달하는데 걸리는 시간
-visited[N] = 0            # 시작점 = 0
+        if x == K:
+            print(visited[x])
+            break
 
-
-while d:
-    x = d.popleft()
-
-    if x == K:
-        print(visited[x])
-        break
-
-    for i in [x-1, x+1, x*2]:
-        if 0 <= i <= 100000 and visited[i] == -1:
-            visited[i] = visited[x] + 1
-            d.append(i)
+        for j in (x-1, x+1, x*2):
+            if 0 <= j <= max_num and not visited[j]:
+                visited[j] = visited[x] + 1
+                q.append(j)
+bfs()
