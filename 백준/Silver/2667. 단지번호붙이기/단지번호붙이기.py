c7eaf1837baf1ec6c141d28214abd2cc582e5acc
g0yujin <136612437+g0yujin@users.noreply.github.com>
2025-08-08 21:37:33 +0900

    [Silver I] Title: 단지번호붙이기, Time: 88 ms, Memory: 109708 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,22 +1,19 @@
 import sys
-sys.setrecursionlimit(10**6)
 
-# 1. 입력 및 초기화
 N = int(sys.stdin.readline())
 
-# 2. 그래프 정보 입력
-graph = [list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
-house = 1
-result = []
+graph = []
+for i in range(N):
+    graph.append(list(map(int, sys.stdin.readline().strip())))
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+house = 1
 
 def dfs(x, y):
     global house
-    graph[x][y] = 0  # 1을 0으로 바꿔서 방문 처리
-
+    graph[x][y] = 0
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
@@ -27,14 +24,15 @@ def dfs(x, y):
 
 
 
+answer = []
 for i in range(N):
     for j in range(N):
         if graph[i][j] == 1:
             dfs(i, j)
-            result.append(house)
+            answer.append(house)
             house = 1
 
-result.sort()
-print(len(result))
-for i in result:
+answer.sort()
+print(len(answer))
+for i in answer:
     print(i)
