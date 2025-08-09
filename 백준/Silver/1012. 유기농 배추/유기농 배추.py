9d85495ad3d74eab83512ea1797908fa6c9f9f9e
g0yujin <136612437+g0yujin@users.noreply.github.com>
2025-08-09 23:55:31 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 114324 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,44 +1,35 @@
-import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
+t = int(input()) # 몇 번의 테스트케이스인지
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
-
-
-def dfs(x, y):
-    graph[x][y] = 0
-
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
-            dfs(nx, ny)
-
-
-# 0.  입력 및 초기화
-T = int(sys.stdin.readline())
-
-for _ in range(T):
-    # 가로, 세로, 배추 위치 개수
-    M, N, K = map(int, sys.stdin.readline().split())
-
-    graph = [[0] * M for _ in range(N)]
-
-    #1. 그래프 정보 입력
-    for i in range(K):
-        a, b = map(int, sys.stdin.readline().split())
-        graph[b][a] = 1
-
-    # 2. dfs 함수 작성
-    answer = 0
-    for i in range(N):
-        for j in range(M):
-            if graph[i][j] == 1:
-                dfs(i, j)
-                answer += 1
+def bfs(graph, x, y): # bfs 함수 구현
+    q = deque()
+    q.append([x, y])
+    graph[x][y] = 0 # 방문 처리; 처리 방식: 표기된 1을 0으로 바꾸기
+    
+    while q: # q가 비었다 = 탐색 가능한 연속 영역 모두 탐색하였다.
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx<m and 0<=ny<n and graph[nx][ny] == 1: # 배추밭 범위를 벗어나지 않고 미방문한 좌표인 경우
+                q.append([nx, ny])
+                graph[nx][ny] = 0
 
 
-    print(answer)
-
+for _ in range(t):  
+    m, n, k = map(int, input().split())
+    graph = [[0]*(n) for _ in range(m)] # 빈 배추밭
+    for _ in range(k):
+        a, b = map(int, input().split())
+        graph[a][b] = 1 # 배추 위치 표기
+        
+    cnt = 0
+    for j in range(m):
+        for k in range(n):
+            if graph[j][k] == 1:
+                bfs(graph, j, k)
+                cnt+=1 # bfs 함수 들어갈때마다 count; 하나의 연속 영역이 끝날 때마다
+    print(cnt)
