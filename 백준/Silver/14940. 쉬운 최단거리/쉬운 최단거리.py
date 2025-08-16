4f88dc2650b634665a8413782c96860139387f4a
g0yujin <136612437+g0yujin@users.noreply.github.com>
2025-08-16 23:03:29 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 260 ms, Memory: 131248 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,40 +1,33 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-from collections import deque
-N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
-visited = [[-1] * M for _ in range(N)]
-dx, dy = [0,0,-1,1], [-1,1,0,0]
+direction = [(-1,0),(1,0),(0,-1),(0,1)]
+n,m = map(int,input().split())
+arr = list(input().split() for _ in range(n))
+visited = [[False]*m for _ in range(n)]
+ans = [[0]*m for _ in range(n)]
+for i in range(n):
+    for j in range(m):
+        if arr[i][j]=="2":
+            visited[i][j] = True
+            q = deque([(i,j)])
 
-def bfs(i,j):
-    queue = deque()
-    queue.append((i,j))
+while q:
+    x, y = q.popleft()
+    for dx, dy in direction:
+        nx = x+dx
+        ny = y+dy
 
-    visited[i][j] = 0
+        if 0<=nx<n and 0<=ny<m and arr[nx][ny]=="1" and not visited[nx][ny]:
+            q.append((nx,ny))
+            visited[nx][ny] = True
+            ans[nx][ny]= ans[x][y]+1
 
-    while queue:
-        x, y = queue.popleft()
-        
-        for i in range(4):
-            nx, ny = dx[i] + x, dy[i] + y
-            
-            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == -1:
-                if graph[nx][ny] == 0: 
-                    visited[nx][ny] = 0
-                elif graph[nx][ny] == 1:
-                    visited[nx][ny] = visited[x][y] + 1
-                    queue.append((nx,ny))
-
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 2 and visited[i][j] == -1:
-            bfs(i,j)
-
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 0:
-            print(0, end=' ')
+for i in range(n):
+    for j in range(m):
+        if not visited[i][j] and arr[i][j]=="1":
+            print(-1,end=" ")
         else:
-            print(visited[i][j], end=' ')
+            print(ans[i][j], end=" ")
     print()
