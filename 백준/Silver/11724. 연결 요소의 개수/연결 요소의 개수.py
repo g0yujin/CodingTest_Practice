6103edfb5dc466a4622fa6829fab003cb2b52e09
g0yujin <136612437+g0yujin@users.noreply.github.com>
2025-08-22 15:30:17 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 328 ms, Memory: 172992 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,28 +1,33 @@
 import sys
-sys.setrecursionlimit(10**6)  # 재귀 깊이 제한을 늘림
-
-
-def dfs(graph, v, visited):
-    visited[v] = True  # 현재 노드(v) 방문 처리
-
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
-
 
 N, M = map(int, sys.stdin.readline().split())
-graph = [[] for _ in range(N+1)]
-for i in range(M):
-    u, v = map(int, sys.stdin.readline().split())
-    graph[u].append(v)
-    graph[v].append(u)
 
-visited = [False] * (N+1)
-count = 0   # 연결 요소의 수
+graph = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+
+for i in range(M):
+    U, V = map(int, sys.stdin.readline().split())
+    graph[U].append(V)
+    graph[V].append(U)
+
+def dfs(v):
+    visited[v] = 1
+
+    for j in graph[v]:
+        if visited[j] == 0:
+            dfs(j)
+
+answer = 0
 
 for i in range(1, N+1):
-    if not visited[i]:
-        dfs(graph, i, visited)
-        count += 1      # dfs가 끝나면 연결 요소의 수 += 1
+    if visited[i] == 0:
+        dfs(i)
+        answer += 1
 
-print(count)
+
+
+print(answer)
+
+
+
+
