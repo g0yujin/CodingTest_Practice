843b6d52299b65e2cd4eafaee498c18bf31be581
g0yujin <136612437+g0yujin@users.noreply.github.com>
2025-09-01 23:32:51 +0900

    [Silver V] Title: 숫자 카드, Time: 444 ms, Memory: 226664 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,16 +1,17 @@
 import sys
 
-N = int(sys.stdin.readline()) # 상근이의 숫자 카드 개수
-card_n = set(map(int, sys.stdin.readline().split())) # N개의 숫자
-M = int(sys.stdin.readline()) # 주어진 숫자 카드 개수
-card_m = list(map(int, sys.stdin.readline().split())) # M개의 숫자
+N = int(sys.stdin.readline())
+cards = list(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline())
+checks = list(map(int, sys.stdin.readline().split()))
 
+_dict = {}  # 속도는 dictionary!
+for i in range(len(cards)):
+    _dict[cards[i]] = 0  # 아무 숫자로 mapping
 
-for i in range(M):
-    if card_m[i] in card_n:
-        card_m[i] = 1
+for j in range(M):
+    if checks[j] not in _dict:
+        print(0, end=' ')
     else:
-        card_m[i] = 0
-
-print(*card_m)
-
+        print(1, end=' ')
+ 
