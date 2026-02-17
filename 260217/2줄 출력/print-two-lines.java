fce0f2ee96bba3afa75cbd1811c69f1dca567539
g0yujin <136612437+g0yujin@users.noreply.github.com>
2026-02-17 20:30:07 +0900

    Title: 2줄 출력, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

--- 260217/2줄 출력/print-two-lines.java
+++ 260217/2줄 출력/print-two-lines.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code her
-        System.out.println("Hello")
-        System.out.println("World")
+        System.out.println("Hello");
+        System.out.println("World");
     }
 }
