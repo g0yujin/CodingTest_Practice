cf3d8a70c950660bafa9c6a94552d409b63a503f
g0yujin <136612437+g0yujin@users.noreply.github.com>
2026-02-17 21:30:38 +0900

    Title: 문자 출력하기, Time: 91ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260217/문자 출력하기/print-text.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        String s = "G";
+        for(int i=0; i<8; i++){
+            System.out.print(s);
+        }
+    }
+}
